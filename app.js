a9dcc463414daea3d033ba94ee38af2a1f06a580
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-12-14 18:57:29 +0530

    Level 2 completed

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const e = require("express");
+const encryption = require("mongoose-encryption");
+require("dotenv").config()
 
 
-
+const Schema = mongoose.Schema;
+const port = 3000;
+const secret = process.env.SECRET;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"))
 app.set("view engine", "ejs")
 
-const port = 3000;
 
 
 mongoose.connect("mongodb://localhost:27017/whisper", { useNewUrlParser: true }, (err) => {
@@ -22,11 +24,11 @@ mongoose.connect("mongodb://localhost:27017/whisper", { useNewUrlParser: true },
 })
 
 
-const userSchema = {
+const userSchema = new Schema({
     email: String,
     password: String,
-}
-
+});
+userSchema.plugin(encryption, { secret: secret, encryptedFields: ['password'] });
 const User = mongoose.model("User", userSchema);
 
 
@@ -52,10 +54,20 @@ app.post("/register", (req, res) => {
     })
 })
 app.post("/login", (req, res) => {
-    User.findOne(req.body, (err,foundUser) => {
-        if (foundUser)
-            res.render("secrets");
+    User.findOne({ email: req.body.email }, (err, foundUser) => {
+        if (foundUser) {
+            // console.log('user found')
+            if (foundUser.password === req.body.password) {
+                // console.log('password matched')
+                res.render("secrets");
+            }
+            else {
+                // console.log('password not matched')
+                res.render('login')
+            }
+        }
         else {
+            // console.log('user not found')
             res.render("login")
         }
     })
